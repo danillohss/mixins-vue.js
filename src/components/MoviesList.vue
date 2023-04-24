3028e4a80aa9518db38fd1ce54317137f5c1258d
Danillo Soares <danillohss@gmail.com>
2023-04-24 20:25:33 -0300

    refatorando mixin

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,29 +1,21 @@
 <template>
   <div>
     <h3>Lista de Filmes</h3>
-    <input type="text" placeholder="Nome do filme..." v-model="movie" />
-    <button type="button" @click="addMovie()">Adicionar Filme</button><br />
+    <input type="text" placeholder="Nome do Filme..." v-model="item" />
+    <button type="button" @click="addItem()">Adicionar Filme</button><br />
     <br />
-    <ul v-for="(movie, index) in movies" :key="index">
+    <ul v-for="(item, index) in items" :key="index">
       <li>
-        {{ movie }}
+        {{ item }}
       </li>
     </ul>
   </div>
 </template>
   
   <script>
+import MixinsList from "@/mixins/MixinsList";
 export default {
   name: "MoviesList",
-  data: () => ({
-    movie: "",
-    movies: [],
-  }),
-  methods: {
-    addMovie() {
-      this.movies.push(this.movie);
-      this.movie = "";
-    },
-  },
+  mixins: [MixinsList],
 };
 </script>
